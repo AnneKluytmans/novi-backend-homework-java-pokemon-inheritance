c8f3a2ba26490bad975c881afcaeee26251c2719
AnneKluytmans <annekluytmans1@gmail.com>
2024-09-27 16:45:36 +0200

    Updated super pokemon class

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -3,14 +3,18 @@ package pokemon;
 public abstract class Pokemon {
     private String name;
     private String type;
+    private float weight;
+    private float height;
     private int level;
     private int hp;
     private int xp;
 
 
-    public Pokemon(String name, String type, int level, int hp, int xp) {
+    public Pokemon(String name, String type, float weight, float height, int level, int hp, int xp) {
         this.name = name;
         this.type = type;
+        this.weight = weight;
+        this.height = height;
         this.level = level;
         this.hp = hp;
         this.xp = xp;
@@ -23,6 +27,14 @@ public abstract class Pokemon {
 
     public String getType() {
         return type;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public float getHeight() {
+        return height;
     }
 
     public int getLevel() {
@@ -43,6 +55,14 @@ public abstract class Pokemon {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
     }
 
     public void setLevel(int level) {
@@ -85,8 +105,19 @@ public abstract class Pokemon {
     }
 
     public void attack(Pokemon opponent) {
+        int damage = (int) (Math.random() * level * 5);
+
         System.out.println(name + " attacks " + opponent.getName() + " with level " + opponent.getLevel() + ".");
-        System.out.println("It's a fierce fight!");
+        System.out.println("It's a fierce fight! " + name + " brings " + damage + " damage to " + opponent.getName() + " with level " + opponent.getName() + ".");
+        opponent.takeDamage(damage);
+        if (opponent.getHp() < 10) {
+            System.out.println(opponent.getName() + " is barely holding on!");
+        }
+
+        if (opponent.getHp() <= 0) {
+            System.out.println(opponent.getName() + " has passed out.");
+        }
+        System.out.println("The fight is over! \n Hp of " + opponent.getName() + " is now: " + opponent.getHp() + ".");
     }
 
 }
